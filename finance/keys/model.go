5672c3c71f0a1580c972d631f7442bf21376bd77
dropthemasquerade <49177445+dropthemasquerade@users.noreply.github.com>
2024-03-17 02:43:28 +0800

    using _

--- finance/keys/model.go
+++ finance/keys/model.go
@@ -20,7 +20,7 @@ const (
 // Model 商品信息
 type Model struct {
 	// 模型继承
-	model.Model `json:"meta" bson:"-"`
+	model.Model `json:"_" bson:"_"`
 	// 基本的数据库模型字段，一般情况所有model都应该包含如下字段
 	// 创建时（用户上传的数据为空，所以默认可以不传该值)
 	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
